e0378f5d90be354df7e513b648725e2340e390fe
Jairo Júnior <ojairo.dev@gmail.com>
2021-06-14 22:22:08 -0300

    add route dashboard

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function Routes(){
   return(
@@ -10,7 +11,7 @@ function Routes(){
       <Switch>
         <Route path='/' exact component={Login}/>
         <Route path='/register' exact component={Register}/>
-        {/* <Route path='/dashboard' exact component={}/> */}
+        <Route path='/dashboard' exact component={Dashboard}/>
       </Switch>
     </BrowserRouter>
   )
