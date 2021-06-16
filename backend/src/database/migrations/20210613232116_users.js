de4c05fabcd5be05762070e59da1745ce1ea4e5d
Jairo Júnior <ojairo.dev@gmail.com>
2021-06-16 18:26:35 -0300

    add field email on db

--- backend/src/database/migrations/20210613232116_users.js
+++ backend/src/database/migrations/20210613232116_users.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', function(table){
     table.string('id').primary().notNullable()
     table.string('name').notNullable()
+    table.string('email').notNullable()
     table.string('user').notNullable()
     table.string('pass').notNullable()
 
