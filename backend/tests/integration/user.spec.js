63b06a861ecdfaf7852029d3cf84dd8478630027
Jairo Júnior <ojairo.dev@gmail.com>
2021-06-17 12:46:30 -0300

    test to insert users

--- backend/tests/integration/user.spec.js
+++ backend/tests/integration/user.spec.js
@@ -1,15 +1,18 @@
 const req = require('supertest')
 const app = require('../../src/app')
 const connection = require('../../src/database/connection')
+const generateUniqueToken = require('../../src/utils/generateUniqueToken')
+const generateUniqueId = require('../../src/utils/generateUniqueId')
+
 const {validate, version} = require('uuid')
 
 describe('Users', () => {
-  beforeEach(async () => {
-    await connection.migrate.rollback()
+  beforeAll(async () => {
     await connection.migrate.latest()
   })
 
   afterAll(async () => {
+    await connection.migrate.rollback()
     await connection.destroy()
   })
 
@@ -23,10 +26,17 @@ describe('Users', () => {
 	    user: 'ojairo',
 	    pass: 'ojairo'
     })
-
     expect(res.body).toHaveProperty('id')
     expect((
       validate(res.body.id) && version(res.body.id) === 4))
       .toBe(true)
+  })
+
+  it('should be able to get all users', async () => {
+    const id = generateUniqueId()
+    const token = generateUniqueToken(id)
+    const {body} = await req(app).get('/users')
+    .set('Authorization', token)
+    expect(body.users[0]).toHaveProperty('id')
   })
 })
